8059ae06a8a6f671b5ab09df3d1becaf0cc4b2ba
Anuj Agarwal <anuj.ism@gmail.com>
2018-03-20 13:17:22 +0530

    added remove blank rows feature

--- d6t/stack/read_excel_adv.py
+++ d6t/stack/read_excel_adv.py
@@ -1,27 +1,21 @@
 from pandas import ExcelFile
 
 
-def read_excel_adv(io, sheet_name=0, index_col=None, usecols=None, names=None, parse_dates=False,
+def read_excel_adv(io, sheet_name=0, header=0, index_col=None, usecols=None, names=None, parse_dates=False,
                    date_parser=None, na_values=None, thousands=None, convert_float=True, converters=None,
                    dtype=None, true_values=None, false_values=None, engine=None, squeeze=False,
-                   userows=None, remove_blank_cols=False, collapse_header=False, header_range=None,
-                   **kwds):
+                   userows=None, remove_blank_cols=False, remove_blank_rows=False, collapse_header=False,
+                   header_range=None, **kwds):
     """
     # TODO: Handle multiple sheets at once. Each sheet may have difference col and rows range.
-    :param names: TODO
     :param userows: string for a single dataset(eg '1:100' means row 1 to row 100)
-    :param remove_blank_cols:
+    :param remove_blank_cols: to remove all blank columns
+    :param remove_blank_rows: to remove all blank rows
     :param collapse_header: Replace newline ('\n') to space
-    :param header_range: list of header range (list of list/int) or a single list of headers or an int
-                          (None for default)
-    :return:
+    :return: dataframe
     """
     if not isinstance(io, ExcelFile):
         io = ExcelFile(io, engine=engine)
-
-    header = 0
-    if header_range:
-        header = header_range
 
     skiprows = 0
     skip_footer = 0
@@ -46,6 +40,8 @@ def read_excel_adv(io, sheet_name=0, index_col=None, usecols=None, names=None, p
         false_values=false_values, squeeze=squeeze, **kwds)
     if remove_blank_cols:
         df = df.dropna(axis='columns', how='all')
+    if remove_blank_rows:
+        df = df.dropna(axis='rows', how='all')
     if collapse_header:
         df.rename(columns=lambda x: x.strip().replace("\n", ' '), inplace=True)
     return df
