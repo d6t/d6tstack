a285bcc16e25b8ce362c411a6d90b9bb3fdaa3b9
Anuj Agarwal <anuj.ism@gmail.com>
2018-03-22 16:27:05 +0530

    read excel adv

--- d6t/stack/read_excel_adv.py
+++ d6t/stack/read_excel_adv.py
@@ -1,47 +1,45 @@
-from pandas import ExcelFile
+from pandas import read_excel
+from openpyxl.utils import coordinate_from_string
 
 
-def read_excel_adv(io, sheet_name=0, header=0, index_col=None, usecols=None, names=None, parse_dates=False,
-                   date_parser=None, na_values=None, thousands=None, convert_float=True, converters=None,
-                   dtype=None, true_values=None, false_values=None, engine=None, squeeze=False,
-                   userows=None, remove_blank_cols=False, remove_blank_rows=False, collapse_header=False,
-                   header_range=None, **kwds):
+def read_excel_adv(io, remove_blank_cols=False, remove_blank_rows=False, collapse_header=False,
+                   header_range=None, header_xls_start=None, header_xls_end=None, **kwds):
     """
     # TODO: Handle multiple sheets at once. Each sheet may have difference col and rows range.
-    :param userows: string for a single dataset(eg '1:100' means row 1 to row 100)
+    :param header_xls_start: string for a single dataset(eg 'B23')
+    :param header_xls_end: string for a single dataset(eg 'I24')
+    :param header_range: string for a single dataset(eg 'B23:I24')
     :param remove_blank_cols: to remove all blank columns
     :param remove_blank_rows: to remove all blank rows
     :param collapse_header: Replace newline ('\n') to space
     :return: dataframe
     """
-    if not isinstance(io, ExcelFile):
-        io = ExcelFile(io, engine=engine)
-
-    skiprows = 0
-    skip_footer = 0
-    if userows:
-        userows = [int(x) for x in userows.split(':')]
-        if sheet_name:
-            total_rows = io.book.sheet_by_name(sheet_name).nrows
+    if not (header_xls_start and header_xls_end):
+        if header_range:
+            header_range = header_range.split(':')
+            header_xls_start , header_xls_end = header_range
         else:
-            total_rows = io.book.sheet_by_index(0).nrows
-        print(total_rows)
-        skiprows = userows[0] - 1
-        last_row = userows[1]
-        # calc number of footer rows
-        skip_footer = total_rows - last_row
+            # Return with error message - Discuss with Norman
+            raise Exception
 
-    df = io.parse(
-        sheet_name=sheet_name, header=header, skiprows=skiprows, names=names,
-        index_col=index_col, usecols=usecols, parse_dates=parse_dates,
-        date_parser=date_parser, na_values=na_values, thousands=thousands,
-        convert_float=convert_float, skip_footer=skip_footer,
-        converters=converters, dtype=dtype, true_values=true_values,
-        false_values=false_values, squeeze=squeeze, **kwds)
+    # header, skiprows, usecols
+    scol, srow = coordinate_from_string(header_xls_start)
+    ecol, erow = coordinate_from_string(header_xls_end)
+
+    header = [x for x in range(erow - srow + 1)]
+    usecols = scol + ":" + ecol
+    skiprows = srow - 1
+
+    df = read_excel(io, header=header, skiprows=skiprows, usecols=usecols, **kwds)
+
     if remove_blank_cols:
         df = df.dropna(axis='columns', how='all')
     if remove_blank_rows:
         df = df.dropna(axis='rows', how='all')
     if collapse_header:
-        df.rename(columns=lambda x: x.strip().replace("\n", ' '), inplace=True)
+        if len(header) > 1:
+            df.columns = [' '.join([s for s in col if not 'Unnamed' in s]).strip().replace("\n", ' ')
+                          for col in df.columns.values]
+        else:
+            df.rename(columns=lambda x: x.strip().replace("\n", ' '), inplace=True)
     return df
