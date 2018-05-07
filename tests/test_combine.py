5f27af701b40a02f90cfd8cc8b65e5e4ab1f2f8e
Anuj Agarwal <anuj.ism@gmail.com>
2018-05-07 10:46:55 +0530

    from master

--- tests/test_combine.py
+++ tests/test_combine.py
@@ -410,11 +410,12 @@ def test_combine_csv(create_files_csv):
 
     r = combine_files(create_files_csv, cfg_fname_base_out_dir, logger, cfg_return_df=False)
     assert r['status'] == 'complete'
-    df = pd.read_csv(cfg_fname_base_out_dir+'/combined.csv', dtype=str, index_col=False)
-    assert df.equals(df2)
-    df_sample = pd.read_csv(cfg_fname_base_out_dir+'/combined-sample.csv', dtype=str)
-    df_sample = df_sample.sort_index(axis=1)
-    assert df_sample.equals(df2.groupby('filename').head(5).set_index('filename').reset_index().sort_index(axis=1))
+    df = pd.read_csv(cfg_fname_base_out_dir + '/combined.csv', dtype=str)
+    assert sorted(df.columns) == sorted(df2.columns)
+    assert df.equals(df2[df.columns])
+    df_sample = pd.read_csv(cfg_fname_base_out_dir + '/combined-sample.csv', dtype=str)
+    assert sorted(df_sample.columns) == sorted(df2.columns)
+    assert df_sample.equals(df2.groupby('filename').head(5).set_index('filename').reset_index()[df_sample.columns])
 
 
 def test_combine_csv_colmismatch(create_files_csv_colmismatch):
